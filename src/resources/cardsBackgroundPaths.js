1af9386c1e69ec70118157fced3f94fe8336709a
Stephane Mbesse <stephane.mbesse@gmail.com>
2022-03-12 15:17:55 +0100

    refactor: rename and handle assets

---
+++ src/resources/cardsBackgroundPaths.js
@@ -0,0 +1,5 @@
+export default {
+  green: "/images/Card-background-Greenish.svg",
+  orange: "/images/Card-background-Orangish.svg",
+  grey: "/images/Card-background-Greyish.png",
+};
---
+++ src/resources/iconsPaths.js
@@ -0,0 +1,6 @@
+export default {
+  list: "/icons/list-view-icons.png",
+  sorting: "/icons/Sorting-icon.png",
+  cards: "/icons/Thumbnail-view-icon.png",
+  search: "/icons/Vector.png",
+};
